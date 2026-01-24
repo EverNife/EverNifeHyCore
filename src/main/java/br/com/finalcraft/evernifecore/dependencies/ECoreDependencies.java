d533b0cf11fcbefe2da4ce0e4e80c2cffc73983c
Pétrus Pradella <petrus_ant@hotmail.com>
2026-01-24 19:55:43 -0300

    Always shadow all dependencies

--- src/main/java/br/com/finalcraft/evernifecore/dependencies/ECoreDependencies.java
+++ src/main/java/br/com/finalcraft/evernifecore/dependencies/ECoreDependencies.java
@@ -5,13 +5,13 @@ import net.byteflux.libby.Library;
 public class ECoreDependencies {
 
     public static void initialize(DependencyManager dependencyManager){
-        dependencyManager.loadLibrary(
-                Library.builder()
-                        .groupId("com.github.Carleslc.Simple-YAML")
-                        .artifactId("Simple-Yaml")
-                        .version("1.8.4")
-                        .relocate("org.yaml","br.com.finalcraft.libs.yaml")
-                        .build()
-        ); //SnakeYAML + Simple-YAML
+//        dependencyManager.loadLibrary(
+//                Library.builder()
+//                        .groupId("com.github.Carleslc.Simple-YAML")
+//                        .artifactId("Simple-Yaml")
+//                        .version("1.8.4")
+//                        .relocate("org.yaml","br.com.finalcraft.libs.yaml")
+//                        .build()
+//        ); //SnakeYAML + Simple-YAML
     }
 }
