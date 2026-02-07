378e396e477a5fb53f9f7e743bc8ce44208941a6
Pétrus Pradella <petrus_ant@hotmail.com>
2026-02-07 05:16:19 -0300

    Create InnerPAPIRegisterer to prevent java.lang.NoClassDefFoundError

--- src/main/java/br/com/finalcraft/evernifecore/integration/placeholders/PAPIIntegration.java
+++ src/main/java/br/com/finalcraft/evernifecore/integration/placeholders/PAPIIntegration.java
@@ -2,11 +2,11 @@ package br.com.finalcraft.evernifecore.integration.placeholders;
 
 import at.helpch.placeholderapi.PlaceholderAPI;
 import at.helpch.placeholderapi.PlaceholderAPIPlugin;
-import at.helpch.placeholderapi.expansion.PlaceholderExpansion;
 import br.com.finalcraft.evernifecore.EverNifeCore;
 import br.com.finalcraft.evernifecore.config.playerdata.IPlayerData;
 import br.com.finalcraft.evernifecore.integration.placeholders.papi.PAPIRegexReplacer;
 import br.com.finalcraft.evernifecore.integration.placeholders.papi.SimplePAPIHook;
+import br.com.finalcraft.evernifecore.integration.placeholders.papi.SimplePlaceholderExpansion;
 import br.com.finalcraft.evernifecore.placeholder.replacer.RegexReplacer;
 import br.com.finalcraft.evernifecore.util.FCColorUtil;
 import br.com.finalcraft.evernifecore.util.FCReflectionUtil;
@@ -14,7 +14,6 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
-import org.jetbrains.annotations.NotNull;
 
 public class PAPIIntegration {
 
@@ -28,44 +27,12 @@ public class PAPIIntegration {
     }
 
     public static <P extends IPlayerData> RegexReplacer<P> createPlaceholderIntegration(@Nonnull JavaPlugin plugin, @Nonnull String pluginBaseID, @Nonnull Class<P> playerDataType){
-
         PAPIRegexReplacer papiRegexReplacer = new PAPIRegexReplacer(playerDataType);
 
-        SimplePAPIHook simplePAPIHook = new SimplePAPIHook(plugin, papiRegexReplacer);
+        //Inner caller prevents 'java.lang.NoClassDefFoundError'
+        InnerPAPIRegisterer.register(plugin, pluginBaseID, papiRegexReplacer);
 
         EverNifeCore.getLog().info("Registering PAPI Hook for the plugin " + plugin.getManifest().getName() + " with prefix '"  + pluginBaseID + "' using Modern PAPI method.");
-        PlaceholderAPIPlugin.instance().localExpansionManager().register(new PlaceholderExpansion() {
-
-            @Override
-            public @Nonnull String getName() {
-                return plugin.getManifest().getName();
-            }
-
-            @Override
-            public @Nonnull String getIdentifier() {
-                return pluginBaseID;
-            }
-
-            @Override
-            public @Nonnull String getAuthor() {
-                return plugin.getManifest().getAuthors().getFirst().getName();
-            }
-
-            @Override
-            public @Nonnull String getVersion() {
-                return plugin.getManifest().getVersion().toString();
-            }
-
-            @Override
-            public String onPlaceholderRequest(PlayerRef player, @NotNull String params) {
-                return simplePAPIHook.onPlaceholderRequest(player, params);
-            }
-
-            @Override
-            public boolean persist() {
-                return true;
-            }
-        });
 
         return papiRegexReplacer.getRegexReplacer();
     }
@@ -76,6 +43,19 @@ public class PAPIIntegration {
         }
 
         return FCColorUtil.colorfy(text);
+    }
+
+    private static class InnerPAPIRegisterer {
+
+        public static void register(JavaPlugin plugin, String pluginBaseID, PAPIRegexReplacer papiRegexReplacer) {
+            SimplePAPIHook simplePAPIHook = new SimplePAPIHook(plugin, papiRegexReplacer);
+            PlaceholderAPIPlugin.instance().localExpansionManager().register(new SimplePlaceholderExpansion(
+                    plugin,
+                    pluginBaseID,
+                    simplePAPIHook
+            ));
+        }
+
     }
 
 }
